152caf008a259b4ac97e3bf4643c9eb084979a8f
MartaStafie <44975162+MartaStafie@users.noreply.github.com>
2018-11-12 17:24:17 +0200

    Add files via upload

---
+++ despre cmmdc.cppcel corect.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+ using namespace std;
+ int a;
+ int b;
+ int main()
+ {
+     cout<<"a"; cin>>a;
+     cout<<"b"; cin>>b;
+         if(a=b) {cout<<"0"; return 0;}
+         if(a>b) {cout<<"0"; return 0;}
+
+
+ }
